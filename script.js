41be5382783e6affd727e4c191998af1e5b84f96
BeBetterGamer <jarivera21@gmail.com>
2021-06-21 18:53:54 -0400

    changes

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 // Fade On Scroll
 
-
-
 function fadeOnScroll(element) {
-  
-
   var element = document.getElementById("section");
   var element2 = document.getElementById("section-two");
   var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
@@ -32,15 +28,37 @@ function fadeOnScroll(element) {
   }
 }
 
-  window.addEventListener("scroll", fadeOnScroll);
+window.addEventListener("scroll", fadeOnScroll);
 
-  function myFunction() {
-    var x = document.getElementById("nav-menu");
-    if (x.className === "nav") {
-      x.className += "responsive";
-    } else {
-      x.className = "nav";
-    }
+function myFunction() {
+  var x = document.getElementById("nav-menu");
+  if (x.className === "nav") {
+    x.className += "responsive";
+  } else {
+    x.className = "nav";
   }
+}
+
+
+// Teri's on scroll
+// $(function(){
+
+
+//   // top scroll function
+//   $(window).scroll(function(){
+//     $(".section").css("opacity", 1 - $(window).scrollTop() / 500);
+//   });
   
+//   $(document).on('scroll', function(){
+//     var currPos = $(document).scrollTop();
+//     var fader = $('.section');
+//     fader.each(function(){
+//       var offset = $(this).offset().top;
+  
+//       if(currPos > offset) {
+//         $(this).css('opacity', 1 - (currPos - offset) / 1500);
+//       }
+//     })
+//   });
+// })
 
